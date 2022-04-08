7c9e67bf0f67380ed2ac8b7ae14acd4638ff233d
vasu <bandaruvasu1@gmail.com>
2022-04-08 22:50:14 +0530

    jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,73 +1,30 @@
-def call(Map params = [:]) {
-    //Start Default Arguments
-    def args = [
-            COMPONENT            : "",
-            LABEL                : "master"
+def call(String COMPONENT, String ) {
 
-    ]
-    args << params
+pipeline {
+    agent any
 
-    pipeline {
-        agent {
-            label params.LABEL
+    stages {
+        stage('compile') {
+            steps {
+                ssh ''
+                'echo  compile'
+                'echo COMPONENT = ${COMPONENT}'
+            }
         }
 
-        stages {
-
-            stage("Labiling Build") {
-                steps {
-                    script {
-                        str = GIT_BRANCH.split('/').last
-                        addShortText background: 'yellow', color: 'black', bordercolor: 'yellow', text: "COMPONENT = ${params.COMPONENT}"
-                        addShortText background: 'yellow', color: 'black', bordercolor: 'yellow', text: "BRANCH = ${str}"
-                        addShortText background: 'yellow', color: 'black', bordercolor: 'yellow', text: "${ENV}"
-
-                    }
-                }
+        stage('code quality') {
+            steps {
+                ssh ''
+                'echo  code quality'
             }
+        }
 
-            stage("COMPILE") {
-                steps {
-                    sh "echo COMPILE"
-                }
+        stage('Test cases') {
+            steps {
+                ssh ''
+                'echo  test cases'
             }
-
-            stage("CODE QUALITY") {
-                steps {
-                    sh "echo CODE QUALITY"
-                    sh "echo COMPONENT = ${params.COMPONENT}"
-                }
-            }
-
-            stage("TEST CASES") {
-                steps {
-                    sh "echo TEST CASES"
-                }
-            }
-
-//
-//                        stage("ARTIFACTORY") {
-//                            when {
-//                                expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true']) }
-//                            }
-//                            steps {
-//                                sh "echo TEST CASES"
-//                                sh "env"
-//                            }
-//                        }
-//                    }
-//
-//                    post {
-//                        always {
-//                            cleanWs()
-//                        }
-//                    }
-
-
         }
     }
-
 }
-
-
-
+}
