99f0622470e607e24143c2e0810784ca8eab20c8
vasu <bandaruvasu1@gmail.com>
2022-04-08 19:26:25 +0530

    jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,8 +1,8 @@
 def call(Map params = [:]) {
     //Start Default Arguments
     def args = [
-          COMPONENT   : '',
-          LABEL       : 'work'
+            COMPONENT: '',
+            LABEL    : 'work'
 
     ]
     args << params
@@ -14,11 +14,11 @@ def call(Map params = [:]) {
 
         stages {
 
-//            stage("Labiling Build") {
-//                steps {
-//                    script {
-//                        str = GIT_BRANCH.split('/').last
-//                        addShortText background: 'yellow', color: 'black', bordercolor: 'yellow', text: "COMPONENT = ${params.COMPONENT}"
+            stage("Labiling Build") {
+                steps {
+                    script {
+                        str = GIT_BRANCH.split('/').last
+                        addShortText background: 'yellow', color: 'black', bordercolor: 'yellow', text: "COMPONENT = ${params.COMPONENT}"
 //                        addShortText background: 'yellow', color: 'black', bordercolor: 'yellow', text: "BRANCH = ${str}"
 //                        addShortText background: 'yellow', color: 'black', bordercolor: 'yellow', text: "${ENV}"
 //
@@ -26,44 +26,46 @@ def call(Map params = [:]) {
 //                }
 //            }
 
-            stage("COMPILE") {
-                steps {
-                    sh "echo COMPILE"
-                }
-            }
+                        stage("COMPILE") {
+                            steps {
+                                sh "echo COMPILE"
+                            }
+                        }
 
-            stage("CODE QUALITY") {
-                steps {
-                    sh "echo CODE QUALITY"
-                    sh "echo COMPONENT = ${params.COMPONENT}"
-                }
-            }
+                        stage("CODE QUALITY") {
+                            steps {
+                                sh "echo CODE QUALITY"
+                                sh "echo COMPONENT = ${params.COMPONENT}"
+                            }
+                        }
 
-            stage("TEST CASES") {
-                steps {
-                    sh "echo TEST CASES"
-                }
-            }
+                        stage("TEST CASES") {
+                            steps {
+                                sh "echo TEST CASES"
+                            }
+                        }
 
 
-            stage("ARTIFACTORY") {
-                when {
-                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true']) }
-                }
-                steps {
-                    sh "echo TEST CASES"
-                    sh "env"
+                        stage("ARTIFACTORY") {
+                            when {
+                                expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true']) }
+                            }
+                            steps {
+                                sh "echo TEST CASES"
+                                sh "env"
+                            }
+                        }
+                    }
+
+                    post {
+                        always {
+                            cleanWs()
+                        }
+                    }
+
+
                 }
             }
         }
-
-            post {
-                always {
-                    cleanWs()
-                }
-            }
-
-
-
     }
 }
