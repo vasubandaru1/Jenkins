9043f6a6b5efeaaea0c3842365b6beb08fd4fdd9
vasu <bandaruvasu1@gmail.com>
2022-04-09 14:54:49 +0530

    jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -51,7 +51,7 @@ def call(Map params = [:]) {
 
             stage('Upload Artifacts') {
                 when {
-                    expression { sh([returnStdout: true, script: "echo ${GIT_BRANCH} | grep tags || true" ]) }
+                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
                 }
                 steps {
                     sh 'echo  test cases'
