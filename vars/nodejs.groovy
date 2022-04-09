f759c8c5832713aae14db64e7baeab46134951bd
vasu <bandaruvasu1@gmail.com>
2022-04-09 08:45:25 +0530

    jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,8 +1,8 @@
 def call(Map params = [:]) {
 
     def args= [
-            COMPONENT      : "",
-            LABEL          : "master"
+            COMPONENT      : '',
+            LABEL          : 'master'
 
     ]
 
