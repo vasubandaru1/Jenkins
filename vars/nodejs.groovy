617c9cb6c0c6fef396899d3941af63e8304be2f4
vasu <bandaruvasu1@gmail.com>
2022-04-09 07:54:49 +0530

    jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,31 +1,36 @@
-def call(String COMPONENT, String LABEL) {
+//def call(String COMPONENT, String LABEL)
+def call(Map params = [:] ) {
+    def args  = [
+            COMPONENT = '',
+            LABEL     = 'master'
+    ]
+    pipeline {
+        agent {
+            label "${LABEL}"
+        }
 
-pipeline {
-    agent {
-     label   "${LABEL}"
+        stages {
+            stage("compile") {
+                steps {
+                    sh "echo COMPONENT = ${COMPONENT}"
+
+                }
+            }
+
+            stage("code quality") {
+                steps {
+                    sh "echo  code quality"
+
+                }
+            }
+
+            stage("Test cases") {
+                steps {
+                    sh "echo  test cases"
+
+                }
+            }
+        }
     }
 
-    stages {
-        stage("compile") {
-            steps {
-                sh  "echo COMPONENT = ${COMPONENT}"
-
-            }
-        }
-
-        stage("code quality") {
-            steps {
-                sh "echo  code quality"
-
-            }
-        }
-
-        stage("Test cases") {
-            steps {
-                sh "echo  test cases"
-
-            }
-        }
-    }
-}
 }
