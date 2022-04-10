8f5321be4717838a6648af91a19d30bf4a2ff518
vasu <bandaruvasu1@gmail.com>
2022-04-10 10:15:48 +0530

    jenkins

--- nodejs/raja.groovy
+++ nodejs/raja.groovy
@@ -1,79 +1,79 @@
-def call(Map params = [:]) {
-
-    def args= [
-            COMPONENT      : '',
-            LABEL          : 'work'
-
-    ]
-
-    args << params
-
-
-    pipeline {
-        agent {
-            label params.LABEL
-        }
-
-        stages {
-
-            stage('Labeling Build') {
-                steps {
-                    script {
-                        str = GIT_BRANCH.split('/').last()
-                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "COMPONENT = ${params.COMPONENT}"
-                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "BRANCH = ${str}"
-                    }
-                }
-            }
-
-
-            stage('nodejs dependecies') {
-                steps {
-                    sh "echo COMPONENT =  ${params.COMPONENT}"
-
-                }
-            }
-
-            stage('code quality') {
-                steps {
-                    sh 'echo  code quality'
-
-                }
-            }
-
-            stage('Test cases') {
-                steps {
-                    sh 'echo  test cases'
-
-
-                }
-            }
-
-            stage('Upload Artifacts') {
-                when {
-                    expression {sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
-                }
-
-                steps {
-                    sh 'echo  test cases'
-                    sh 'env'
-
-
-                }
-            }
-
-
-        }
-
-        post {
-            always {
-                cleanWs()
-            }
-        }
-    }
-}
-
-//vasu
-
-
-
+//def call(Map params = [:]) {
+//
+//    def args= [
+//            COMPONENT      : '',
+//            LABEL          : 'work'
+//
+//    ]
+//
+//    args << params
+//
+//
+//    pipeline {
+//        agent {
+//            label params.LABEL
+//        }
+//
+//        stages {
+//
+//            stage('Labeling Build') {
+//                steps {
+//                    script {
+//                        str = GIT_BRANCH.split('/').last()
+//                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "COMPONENT = ${params.COMPONENT}"
+//                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "BRANCH = ${str}"
+//                    }
+//                }
+//            }
+//
+//
+//            stage('nodejs dependecies') {
+//                steps {
+//                    sh "echo COMPONENT =  ${params.COMPONENT}"
+//
+//                }
+//            }
+//
+//            stage('code quality') {
+//                steps {
+//                    sh 'echo  code quality'
+//
+//                }
+//            }
+//
+//            stage('Test cases') {
+//                steps {
+//                    sh 'echo  test cases'
+//
+//
+//                }
+//            }
+//
+//            stage('Upload Artifacts') {
+//                when {
+//                    expression {sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+//                }
+//
+//                steps {
+//                    sh 'echo  test cases'
+//                    sh 'env'
+//
+//
+//                }
+//            }
+//
+//
+//        }
+//
+//        post {
+//            always {
+//                cleanWs()
+//            }
+//        }
+//    }
+//}
+//
+////vasu
+//
+//
+//
